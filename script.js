d92ab48ab1570a7b5d7757b14fe9bb2798963b42
Andres Medina <andresdmb96@gmail.com>
2020-02-08 13:15:01 -0400

    Modified

--- script.js
+++ script.js
@@ -1,7 +1,9 @@
 var css = document.querySelector("h3");
 var color1 = document.querySelector(".color1");
 var color2 = document.querySelector(".color2");
+var color3 = document.querySelector(".color3");
 var body = document.getElementById("gradient");
+var button = document.querySelector("button");
 
 function setGradient() {
     body.style.background = 
@@ -9,10 +11,31 @@ function setGradient() {
     + color1.value 
     + ", " 
     + color2.value 
-    + ")";
+    + ", " 
+    + color3.value
+     ")";
 
     css.textContent = body.style.background + ";";
 }
 
+
+/*Not working yet!*/
+function copyElement(){
+    if(setGradient){
+        /*Getting the textfield*/ 
+    var copyText = css.textContent;
+        /*Selecting the text field */
+    copyText.select();
+    copyText.setSelectionRange(0,99999);
+    /*Copying text*/
+    document.execCommand("copy");
+    alert("Copied!");
+    }
+}
+
+
 color1.addEventListener("input",setGradient);
 color2.addEventListener("input",setGradient);
+color3.addEventListener("input",setGradient);
+
+button.addEventListener("click",copyElement);
